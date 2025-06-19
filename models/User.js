7f0556d846f149d8b0f498d3d1cf2a3965c263d1
Mueen <hejiy2002@gmail.com>
2025-06-19 20:27:25 +0300

    creating user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+
+
+const mongoose = require("mongoose");
+
+
+const UserSchema= new mongoose.Schema ({
+
+    username : {type:String ,
+        required :true
+    } ,
+
+   hashedPassword: {
+    type: String,
+    required: true,
+  },
+  isAdmin : {
+    type : Boolean,
+    required :true
+  }
+
+})
+
+module.exports = mongoose.model("User",UserSchema)
